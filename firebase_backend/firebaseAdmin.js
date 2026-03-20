57d24a3ca35d8f0783328b5be46fac89130e6b65
chokolattee <marypauline303@gmail.com>
2026-03-20 21:28:59 +0800

    service

--- firebase_backend/firebaseAdmin.js
+++ firebase_backend/firebaseAdmin.js
@@ -1,8 +1,30 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccountKey.json');
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
+let serviceAccount;
+
+if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+  try {
+    serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+    console.log('Firebase initialized using environment variable');
+  } catch (error) {
+    console.error('Failed to parse FIREBASE_SERVICE_ACCOUNT:', error.message);
+    throw error;
+  }
+} else {
+
+  try {
+    serviceAccount = require('./serviceAccountKey.json');
+    console.log('Firebase initialized using local file');
+  } catch (error) {
+    console.error('serviceAccountKey.json not found. Make sure it exists in the firebase_backend folder');
+    throw error;
+  }
+}
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+  });
+}
 
 module.exports = admin;
